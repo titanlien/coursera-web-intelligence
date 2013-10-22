e8462e8b66fd2c84f52ced9c48c80a00737ddcce
Titan <titan.lien@gmail.com>
2013-10-22 09:50:45 +0800

    temp

--- hw3.py
+++ hw3.py
@@ -1,21 +1,45 @@
 #!/usr/bin/env python
 import mincemeat
-from os import walk
-mypath = "./hw3data"
-data = []
+import glob
 
-for (dirpath, dirnames, filenames) in walk(mypath):
-    for filename in filenames:
-        with open(mypath + "/" + filename) as f:
-            data = f.readlines()
-        data.extend(data)
+filePath = './hw3data/*'
+text_files = glob.glob(filePath)
 
-# The data source can be any dictionary-like object
-source = dict(enumerate(data))
+def file_contents(file_name):
+    f = open(file_name)
+    try:
+        return f.read()
+    finally:
+        f.close()
+
+source = dict((file_name, file_contents(file_name))
+        for file_name in text_files)
+
 
 def mapfn(key, value):
-    from stopwords import allStopWords
-    for line in value.split():
+   stop_words=['all', 'herself', 'should', 'to', 'only', 'under', 'do', 'weve',
+            'very', 'cannot', 'werent', 'yourselves', 'him', 'did', 'these',
+            'she', 'havent', 'where', 'whens', 'up', 'are', 'further', 'what',
+            'heres', 'above', 'between', 'youll', 'we', 'here', 'hers', 'both',
+            'my', 'ill', 'against', 'arent', 'thats', 'from', 'would', 'been',
+            'whos', 'whom', 'themselves', 'until', 'more', 'an', 'those', 'me',
+            'myself', 'theyve', 'this', 'while', 'theirs', 'didnt', 'theres',
+            'ive', 'is', 'it', 'cant', 'itself', 'im', 'in', 'id', 'if', 'same',
+            'how', 'shouldnt', 'after', 'such', 'wheres', 'hows', 'off', 'i',
+            'youre', 'well', 'so', 'the', 'yours', 'being', 'over', 'isnt',
+            'through', 'during', 'hell', 'its', 'before', 'wed', 'had', 'lets',
+            'has', 'ought', 'then', 'them', 'they', 'not', 'nor', 'wont',
+            'theyre', 'each', 'shed', 'because', 'doing', 'some', 'shes',
+            'our', 'ourselves', 'out', 'for', 'does', 'be', 'by', 'on',
+            'about', 'wouldnt', 'of', 'could', 'youve', 'or', 'own', 'whats',
+            'dont', 'into', 'youd', 'yourself', 'down', 'doesnt', 'theyd',
+            'couldnt', 'your', 'her', 'hes', 'there', 'hed', 'their', 'too',
+            'was', 'himself', 'that', 'but', 'hadnt', 'shant', 'with', 'than',
+            'he', 'whys', 'below', 'were', 'and', 'his', 'wasnt', 'am', 'few',
+            'mustnt', 'as', 'shell', 'at', 'have', 'any', 'again', 'hasnt',
+            'theyll', 'no', 'when','other', 'which', 'you', 'who', 'most',
+            'ours ', 'why', 'having', 'once','a','-','.',',']
+   for line in value.splitlines():
         word=line.split(':::')  
         authors=word[1].split('::')  
         title=word[2]
